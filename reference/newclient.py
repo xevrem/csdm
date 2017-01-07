bbb571d0e37f5c9ff64c04c14c848c94c37d90a7
erika jonell <erika.jonell@gmail.com>
2017-01-07 15:33:22 -0500

    work moving from zmq to paws

---
+++ reference/newclient.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+"""
+    This file is part of CSDM.
+
+    Copyright (C) 2013, 2014, 2015, 2016  Erika V. Jonell <@xevrem>
+
+    CSDM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+opts = """CSDM Client
+
+Usage:
+    client.py -h | --help
+    client.py get-file <manifest> <passphrase> [-a <address> -p <port>]
+    client.py list-manifests [-a <address -p <port>]
+    client.py get-manifest <manifest> [-a <address -p <port>]
+    client.py get-value <key> [-a <address> -p <port>]
+    client.py process-file <file>
+    client.py ping [-a <address> -p <port>]
+    client.py status
+    client.py gen-key <passphrase>
+    client.py store-file <manifest> [-a <address> -p <port>]
+    client.py assemble-file <manifest> <passphrase>
+    client.py friend-request [-a <address> -p <port>]
+
+Options:
+    -h --help          Show this screen.
+    -a=<address>        CSDM node address to query [default: 127.0.0.1]
+    -p=<port>           CSDM node port to use in query [default: 8080]
+    -d=<database>       Database to use [default: nodes.db]
+    -m=<manifest-key>   Key for manifest of file to retrieve
+"""
+
+from docopt import docopt
+import sys
+import base64
+import json
+from csdm import make_value_request, make_manifest_request, make_manifest_list_request, make_ping_request, make_friend_request
+import os
+from filer import Filer, MANIFESTDIR, CHUNKDIR, ASSEMBLEDIR, check_dirs, CHUNKSIZE
+from eccpgp import ECCPGP, b64enc, b64dec
+from diffie import DiffieHash
+
+import asyncio
+import uvloop
+from paws.paws import get
+from paws.pahttp import HttpRequest
+
+fi = Filer()
+
+async def do_get_manifest(key):
+    print('C: requesting manifest for {}'.format(key))
+    msg = json.dumps(make_manifest_request(key))
+    data = await get(url='http://localhost/manifest', port=8080, body=msg)
+    req = HttpRequest(data)
+    print('C: manifest retrieved:')
+    #print('\n{}'.format(req.body))
+    return req.body
+
+async def do_get_manifest_list():
+    print('C: requesting manifest list...')
+    data = await get(url='http://localhost/manifest_list', port=8080)
+    req = HttpRequest(data)
+    print('C: manifest list retrieved...')
+    js = json.loads(req.body)
+    for manifest in js['body']:
+        print(manifest)
+
+async def do_get_value(key):
+    #print('C: requesting value...')
+    msg = json.dumps(make_value_request(key))
+    data = await get(url='http://localhost/value', port=8080, body=msg)
+    req = HttpRequest(data)
+    #print('C: value retrieved:')
+    #print('{}'.format(req.body))
+    return req.body
+
+def execute_async_cmd(coro, *args):
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(coro(*args))
+    loop.run_until_complete(future)
+    #loop.run_forever()
+
+async def get_file(manifest_key, passphrase):
+    data = await do_get_manifest(manifest_key)
+    js = json.loads(data)
+    manifest = json.loads(js['body'])
+
+    print('C: manifest retrieved...')
+
+    #setup epgp object
+    epgp = ECCPGP()
+    epgp.generate(passphrase)
+
+    #dectrypt the manifest
+    manenc = manifest['manifest']
+    mandec = epgp.raw_dec(b64dec(manenc), passphrase.encode())
+    manifest['manifest'] = json.loads(mandec.decode())
+
+    print(manifest['manifest'])
+
+    data = b''
+
+    #retrieve each chunk and store it...
+    for chunk in manifest['manifest']:
+        cdata = await do_get_value(chunk)
+        jdata = json.loads(cdata)
+        data = b64dec(jdata['body'])
+
+        fi.write_chunk(data, chunk)
+
+    print('C: got chunks...')
+
+def main(args):
+    #print(args)
+
+    if args['list-manifests']:
+        execute_async_cmd(do_get_manifest_list)
+    elif args['get-manifest']:
+        if args['<manifest>'] is not None:
+            execute_async_cmd(do_get_manifest, args['<manifest>'])
+        else:
+            print('usage: ./client.py get-manifest <manifest>')
+    elif args['get-file']:
+        if args['<manifest>'] is not None:
+            execute_async_cmd(get_file, args['<manifest>'], args['<passphrase>'])
+        else:
+            print('usage: ./clinet.py get-file <manifest> <passphrase>')
+    elif args['get-value']:
+        if args['<key>'] is not None:
+            execute_async_cmd(do_get_value, args['<key>'])
+        else:
+            print('usage: ./client.py get-value <key>')
+    else:
+        print('you done sumfin wrong...')
+
+if __name__ == '__main__':
+    args = docopt(opts)
+    #asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    main(args)
---
+++ reference/newserv.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+"""
+    This file is part of CSDM.
+
+    Copyright (C) 2013, 2014, 2015, 2016  Erika V. Jonell <@xevrem>
+
+    CSDM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+opts = """CSDM Server
+
+Usage:paws
+    server.py -h | --help
+    server.py [-a <address> -p <port> -d <database>]
+
+Options:
+    -h --help      Show this screen.
+    -a=<addres>     Host address to use [default: 127.0.0.1]
+    -p=<port>       Port to use [default: 8080]
+    -d=<database>   Database to use [default: nodes.db]
+"""
+
+from docopt import docopt
+import hashlib
+import csdm
+#from csdm import CSDM, make_status_response
+import signal
+import json
+from filer import Filer, check_dirs
+import base64
+import os
+from diffie import DiffieHash
+import asyncio
+
+from paws.paws import run_server
+from paws.pahttp import HttpResponse
+from paws.palog import AsyncLogger
+
+
+fi = Filer()
+global log, manifest
+manifests = None
+log = None
+
+def cache_manifest_names():
+    global manifests
+
+    tups = os.walk('manifests')
+    tup = tups.__next__()
+    manifests = tup[2]
+    print(manifests)
+
+def package(js):
+    return dumps(js)
+
+async def root(req, res):
+    return res
+
+async def manifest(req, res):
+    global log
+
+    if req.body is not None:
+        js = json.loads(req.body)
+        log.log('S: manifest request received:\n{}'.format(js))
+        key = js['body']
+
+        if not fi.manifest_exists(key):
+            res.body = json.dumps(csdm.make_status_response('NO_FILE'))
+            return res
+
+        manifest = fi.read_manifest_file(key)
+        resjs = csdm.make_status_response('OK')
+        resjs['body'] = manifest
+        res.body = json.dumps(resjs)
+
+        return res
+    else:
+        res.body = json.dumps(csdm.make_status_response('BAD'))
+        return res
+
+async def value(req, res):
+    global log
+
+    if req.body is not None:
+        js = json.loads(req.body)
+        log.log('S: value request received:\n{}'.format(js))
+        key = js['body']
+
+        if not fi.chunk_exists(key):
+            res.body = json.dumps(csdm.make_status_response('NO_FILE'))
+            return res
+
+        chunk = fi.read_chunk(key)
+        rdata = csdm.make_status_response('OK')
+        rdata['body'] = base64.b64encode(chunk).decode()
+        res.body = json.dumps(rdata)
+
+        log.log('S: sending value...')
+
+        return res
+    else:
+        res.body = json.dumps(csdm.make_status_response('BAD'))
+        return res
+
+async def manifest_list(req, res):
+    global manifests
+    global log
+
+    resjs = csdm.make_status_response('OK')
+    resjs['body'] = manifests
+    res.body = json.dumps(resjs)
+    log.log('S: sending manifest list...')
+    return res
+
+async def ping(req, res):
+    return res
+
+async def friend_request(req, res):
+    return res
+
+def routes(app):
+    app.add_route('/', root)
+    app.add_route('/manifest', manifest)
+    app.add_route('/value', value)
+    app.add_route('/manifest_list', manifest_list)
+    app.add_route('/ping', ping)
+    app.add_route('/friend_request', friend_request)
+
+def main():
+    global log
+
+    #determine command line arguments
+    args = docopt(opts)
+
+    #ensure all the directories are properly setup
+    check_dirs()
+
+    #cache manifest IDs
+    cache_manifest_names()
+
+    log = AsyncLogger(loop=asyncio.get_event_loop(), debug=True)
+
+    #start the server
+    run_server(routing_cb=routes, host=args['-a'], port=int(args['-p']), processes=1, use_uvloop=False, debug=False)
+
+
+if __name__ == '__main__':
+    main()
